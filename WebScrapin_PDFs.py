8919151bfcbc0d7ed7734cfd01bd4114ca64e473
MarianaV <marianaverli@hotmail.com>
2025-10-10 09:32:19 -0700

    V4

--- WebScrapin_PDFs.py
+++ WebScrapin_PDFs.py
@@ -7,24 +7,34 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import threading
 from threading import Thread
 from selenium.webdriver.chrome.options import Options
+from urllib.parse import unquote, urlparse, parse_qs
+import urllib3
 
 #Abrir archivo 
-df = pd.read_csv(r"C:/Users\maria\OneDrive\Documents\UNIIE\URLS.csv",header=0)
-
-print(df)
+# df = pd.read_csv(r"C:/Users\maria\OneDrive\Documents\UNIIE\URLS.csv",header=0)
+df = pd.read_csv(r"C:/Users\maria\OneDrive\Documents\UNIIE\PDFS2.csv",header=0)
+#print(df)
 df_p=pd.DataFrame(df)
-# all_links=df_p["URL"]
-all_links = df['URL'].dropna().astype(str).tolist()
-prueba=all_links[151:] 
+all_links=df_p["article_url"].dropna().astype(str).tolist()
+# all_links = df['URL'].dropna().astype(str).tolist()
+prueba=all_links[551:650]#Listo para continuar 10 de oct 
 # ejemplo=url.iloc[0]
 # print(all_links)
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/140.0.0.0 Safari/537.36",
+    # "Referer": url
+}
+
 
 chrome_options = Options()
 
@@ -40,13 +50,14 @@ lock = threading.Lock()
 #Inicializar el navegador 
 # driver=webdriver.Chrome()
 def scrape_pdf(url):
+    #driver=webdriver.Chrome()
     driver=webdriver.Chrome(chrome_options)
     links=[]
     # for n,lin in enumerate[all_links]:
     #Abrir página
     driver.get(url)
     print(f"Hilo {threading.current_thread().name} abriendo: {url}")
-    time.sleep(10)
+    time.sleep(20)
 
     scrolldown = driver.execute_script(
         "window.scrollTo(0, document.body.scrollHeight);var scrolldown=document.body.scrollHeight;return scrolldown;")
@@ -60,7 +71,9 @@ def scrape_pdf(url):
             match = True
 
     time.sleep(5)
-    pdfs = driver.find_elements("class name", "galley-link")
+    pdfs1 = driver.find_elements("class name", "galley-link")
+    pdfs2 = driver.find_elements("xpath", "//a[contains(@class, 'obj_galley_link') and contains(@class, 'pdf')]")
+    pdfs=pdfs1+pdfs2
     for pdf in pdfs:
         p = pdf.get_attribute('href')
         if p:
@@ -70,18 +83,18 @@ def scrape_pdf(url):
     with lock:
         resultados_totales.extend(links)
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 if __name__ == "__main__":
-    hilos = []
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        futures = {executor.submit(scrape_pdf, url): url for url in prueba}
 
-    for url in prueba:
-        # Se puede añadir un nombre al hilo para mejor identificación
-        hilo = threading.Thread(target=scrape_pdf, args=(url,), name=f"WebDriver-{url.split('//')[1].split('/')[0]}")
-        hilos.append(hilo)
-        hilo.start() # Inicia la ejecución del hilo
-
-    # Espera a que todos los hilos terminen
-    for hilo in hilos:
-        hilo.join()
+        for future in as_completed(futures):
+            url = futures[future]
+            try:
+                future.result()
+            except Exception as e:
+                print(f"Error en {url}: {e}")
 
     print("\nTodos los PDFs encontrados:")
     for r in resultados_totales:
@@ -96,7 +109,7 @@ os.chdir(r"C:/Users\maria\OneDrive\Documents\UNIIE")
 df_pdfs = pd.DataFrame(resultados_totales)
 print(df_pdfs)
 # df_pdfs.to_csv('PDFS_CORREOS.csv', header=True, index=True)
-df_pdfs.to_csv('PDFS_CORREOS.csv', mode="a", header=False, index=True)
+df_pdfs.to_csv('PDFS_CORREOS2.csv', mode="a",header=False, index=True)
 
 # correos=[]
 # #Extracción de correos
